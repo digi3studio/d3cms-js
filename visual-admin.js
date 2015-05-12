961511c77f43b782138e63cfc48586a63b2997df
Colin Leung <colin@digi3studio.com>
2015-05-12 17:55:07 +0800

    inject visual admin javascript from visual-admin.js rather than load visual-admin-preview.js in iframe

--- visual-admin.js
+++ visual-admin.js
@@ -21,6 +21,44 @@ $(document).ready(function(){
     });
 
     //load preview content
-    var previewFrame = $('#inline-preview iframe');
+    var previewFrame = $('#inline-preview-frame');
+    var previewCSS = previewFrame.data('preview-css');
+    previewFrame.on('load',function(){
+       $(this).contents().find('body').append(
+           '<link type="text/css" href="'+previewCSS+'" rel="stylesheet" media="all">'
+       );
+
+       $(this).contents().find('a').on('click',disableLinks);
+       $(this).contents().find('span.admin-item').on('click',startEditField);
+       $(this).contents().find('body').on('click',highlightEditField);
+    });
+
     previewFrame.attr('src',previewFrame.data('preview-url'));
 });
+
+function disableLinks(e){
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+}
+
+function startEditField(e){
+    var id = $(this).data('id');
+    var hiddenField = $(window.parent.document.getElementById(id));
+    var newText = prompt("edit text", hiddenField.val());
+
+    if(newText!=null && newText!=hiddenField.val()){
+        hiddenField.val(newText);
+        window.parent.store_fields();
+    }
+}
+
+function highlightEditField(e){
+    if(!$(e.target).hasClass('admin-item')){
+        var body = $(this);
+        body.addClass('show-admin-item');
+        setTimeout(function(){
+            body.removeClass('show-admin-item')
+        },1000);
+    }
+}
