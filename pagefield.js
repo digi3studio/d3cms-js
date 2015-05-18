80ec6155aed6e27c31ee6aafde66d658cb91984f
Colin Leung <colin@digi3studio.com>
2015-05-18 17:31:05 +0800

    gallery photo sorting

--- pagefield.js
+++ pagefield.js
@@ -303,3 +303,26 @@ function add_field_group(e){
     store_fields();
     return false;
 }
+
+/*gallery sort image view*/
+function toggle_image_sortable() {
+    var mode = $('[data-type="22"]').hasClass('image_sortable');
+    if(!mode) {
+        $('[data-type="22"]').addClass('image_sortable');
+        $('input.imagefield.textfield').each(function( index ) {
+            var image = $(this).val()
+            $(this).closest('tr').css('background', 'url(/imagefly/w160-h160-c/media/upload/' + image + ')');
+        });
+    }
+    else {
+        $('[data-type="22"]').removeClass('image_sortable');
+        $('input.imagefield.textfield').each(function( index ) {
+
+            $(this).closest('tr').css('background', 'none');
+        });
+    }
+}
+
+function add_toggle_image_sortable_switch () {
+    $('#count22').after('<a onclick="toggle_image_sortable()">#</a>');
+}
